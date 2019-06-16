e1d4165a6ac56c5e8f8dca686f96a1eede45d40a
Francesco Di Sales <51760933+FrancescoDiSales@users.noreply.github.com>
2019-06-16 17:08:55 +0200

    Updated test usage file

--- test.cpp
+++ test.cpp
@@ -9,13 +9,17 @@ int main(int argc, char** argv)
 {
 	
 	
-		JSONR json("defJSON");
+		JSONR json("def2JSON");
 		string nomi[3] = {"giovanni","antonio","michele"};
 		string numeri[3] ={"1","2","3"};
 		cout<<"test JSON"<<endl;
 
 		json.addString("hello","hello world");
 		json.addInteger("numero","2");
+		json.addBool("active","false");
+		json.addBool("active3","true");
+		json.addBool("active2","banana");
+		
 		json.addArray("persone",nomi,3);
 		json.addArray("numeri",numeri,3);
 		json.addObject("obj",nomi,numeri,3,3);
